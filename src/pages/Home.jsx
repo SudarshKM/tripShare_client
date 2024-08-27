bef47630674a8d19c5f2372cbd86b884546095fc
sudarshkm <sudarshkm85@gmail.com>
2024-08-27 23:39:27 +0530

    daily routine

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -194,12 +194,12 @@ function Home() {
               className="search d-flex justify-content-center p-md-2 shadow mt-2 rounded"
               style={{ border: "1px solid darkslateblue" }}
             >
-              <input
+            { component === "Feedcard" ? <input
                 onChange={(e) => setSearchKey(e.target.value)}
                 type="text"
                 placeholder="Destination"
                 className="m-2 rounded form-control"
-              />
+              /> : null}
               {/* <button className='btn btn-info rounded-5 mx-3'><FontAwesomeIcon icon={faMagnifyingGlass} /></button> */}
             </div>
           </div>
